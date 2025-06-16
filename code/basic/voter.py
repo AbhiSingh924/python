509f4627c53cf8f4dc01a88707d01fe88ba4942f
Abhishek <abhi5678raj@gmail.com>
2025-06-16 21:59:49 +0530

    this code demonstrate basic implementation of input and logical operations.

---
+++ code/basic/voter.py
@@ -0,0 +1,8 @@
+name = input("Please inter you name :- ")
+age = int(input("Enter your Age :- "))
+
+if age >= 18:
+    print(f"Hello {name} you are a valid voter 😊.")
+    
+else:
+    print(f"Hello {name} you are not a valid voter 😒.")
