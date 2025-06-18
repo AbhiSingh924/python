0b5260c3d8bd2a1dff27b83a22081753d88ecf4c
Abhishek <abhi5678raj@gmail.com>
2025-06-18 23:59:09 +0530

    This simple python code finds all the factors or a number

---
+++ code/basic/for/factors.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number:- "))
+factors = ""
+for i in range(1, num+1):
+    if num % i == 0:
+        factors += str(i) + " "
+    
+print(f"All the Factors of {num} are:- {factors} ")
